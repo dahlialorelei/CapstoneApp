09caba76d16875ffa62b68a3404b6e3847a1eec1
dahlialorelei <113202608+dahlialorelei@users.noreply.github.com>
2024-11-05 17:32:19 -0500

    Initial Commit

---
+++ CapstoneApp/CapstoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneAppApp.swift
+//  CapstoneApp
+//
+//  Created by Dahlia Taylor on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapstoneAppTests/CapstoneAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneAppTests.swift
+//  CapstoneAppTests
+//
+//  Created by Dahlia Taylor on 2024-11-05.
+//
+
+import Testing
+@testable import CapstoneApp
+
+struct CapstoneAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
